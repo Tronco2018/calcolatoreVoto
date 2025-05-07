d2c92a0e926d9acf0f4e5886a97acd3d3c3633a6
Tronco2018 <tronco2018dev@gmail.com>
2025-05-07 10:13:14 +0200

    Minor issue fixing

--- math1.js
+++ math1.js
@@ -1,10 +1,18 @@
 function calculate(){
-	var totalizzato = parseInt(document.getElementById("totalizzato").value, 10);
-	var votoMax = parseInt(document.getElementById("votoMassimo").value, 10);
-	var votoMin = parseInt(document.getElementById("votoMinimo").value, 10);
-	var punteggioMax = parseInt(document.getElementById("punteggiomax").value, 10);
-	let result = (totalizzato/punteggioMax) * (votoMax-votoMin) + votoMin;
-
-	document.getElementById("resultLabel").innerText = result;
-	
+	document.getElementById("errorLabel").innerText = "";
+	var totalizzato = parseFloat(document.getElementById("totalizzato").value, 10);
+	var votoMax = parseFloat(document.getElementById("votoMassimo").value, 10);
+	var votoMin = parseFloat(document.getElementById("votoMinimo").value, 10);
+	var punteggioMax = parseFloat(document.getElementById("punteggiomax").value, 10);
+	let result = ((totalizzato/punteggioMax) * (votoMax-votoMin) + votoMin) * 10;
+	let arrotondato = Math.round(result) / 10;
+	if (totalizzato > punteggioMax){
+		document.getElementById("errorLabel").innerText = "\nPunteggio totalizzato maggiore del possibile!";
+	} else if (totalizzato < 0){
+		document.getElementById("errorLabel").innerText = "\nIl punteggio totalizzato deve essere maggiore di zero";
+	} else if (Number.isNaN(arrotondato)){
+		document.getElementById("errorLabel").innerText = "\nUno o + campi sono vuoti, riprovate";	
+	} else {
+		document.getElementById("resultLabel").innerText = arrotondato;
+	}
 }
